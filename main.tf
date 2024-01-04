f66a87a597ad773b7b28cce0851e8938e1b91554
demouser@gmail.com <demo user>
2024-01-04 12:22:05 +0000

    Initial files added

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ module "spoke" {
     source = "value"
     subscription_id_management = var.MANAGEMENT_SUBSCRIPTION_ID
 }
+module "spoke" {
+    source = "value"
+    subscription_id_management = var.MANAGEMENT_SUBSCRIPTION_ID
+}
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+}
