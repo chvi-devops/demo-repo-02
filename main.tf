3161b6dcb9ac42c98484e97f99ef8c58ae4d7c34
demouser@gmail.com <demo user>
2024-01-04 11:31:32 +0000

    Initial files added

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ module "spoke" {
     source = "value"
     subscription_id_management = var.MANAGEMENT_SUBSCRIPTION_ID
 }
+module "spoke" {
+    source = "value"
+    subscription_id_management = var.MANAGEMENT_SUBSCRIPTION_ID
+}
